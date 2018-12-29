dbf17489ba4f6c931b14313d264466f972ce5207
dogyeong <shroad1802@naver.com>
2018-12-29 14:53:02 +0900

    초기 레이아웃

--- app/src/main/java/com/doongsil/button/button/MainActivity.java
+++ app/src/main/java/com/doongsil/button/button/MainActivity.java
@@ -1,13 +1,92 @@
 package com.doongsil.button.button;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewManager;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.doongsil.button.button.R;
 
 public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
+    FragmentManager fm;
+    FragmentTransaction tran;
+    Menu1 Menu1;
+    Menu2 Menu2;
+    Menu3 Menu3;
+    Menu4 Menu4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        BottomNavigationViewHelper.removeShiftMode(navigation);
+        Menu1 = new Menu1();
+        Menu2 = new Menu2();
+        Menu3 = new Menu3();
+        Menu4 = new Menu4();
+        setFrag(0);
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener
+            mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener(){
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    setFrag(0);
+                    return true;
+                case R.id.navigation_dashboard:
+                    setFrag(1);
+                    return true;
+                case R.id.navigation_notifications:
+                    setFrag(2);
+                    return true;
+                case R.id.navigation_settings:
+                    setFrag(3);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    public void setFrag(int n) {
+        fm = getFragmentManager();
+        tran = fm.beginTransaction();
+        switch (n){
+            case 0:
+                tran.replace(R.id.main_frame, Menu1);  //replace의 매개변수는 (프래그먼트를 담을 영역 id, 프래그먼트 객체) 입니다.
+                tran.commit();
+                break;
+            case 1:
+                tran.replace(R.id.main_frame, Menu2);  //replace의 매개변수는 (프래그먼트를 담을 영역 id, 프래그먼트 객체) 입니다.
+                tran.commit();
+                break;
+            case 2:
+                tran.replace(R.id.main_frame, Menu3);  //replace의 매개변수는 (프래그먼트를 담을 영역 id, 프래그먼트 객체) 입니다.
+                tran.commit();
+                break;
+            case 3:
+                tran.replace(R.id.main_frame, Menu4);  //replace의 매개변수는 (프래그먼트를 담을 영역 id, 프래그먼트 객체) 입니다.
+                tran.commit();
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/doongsil/button/button/Menu3.java
@@ -0,0 +1,19 @@
+package com.doongsil.button.button;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Menu3 extends Fragment {
+    View view;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.menu3, container, false);
+
+        return view;
+    }
+}
